26536dedd68dfadd276e967aa995e74d6486c341
Mario Mauricette <mmauri04@hotmail.fr>
2015-11-03 10:44:15 +0100

    Update devinette.js
    changement de script

--- js/devinette.js
+++ js/devinette.js
@@ -11,39 +11,27 @@ console.log("Bienvenue dans ce jeu de devinette !");
 var solution = Math.floor(Math.random() * 100) + 1;
 
 // Décommentez temporairement cette ligne pour mieux vérifier le programme
-// console.log("(La solution est " + solution + ")");
+//console.log("(La solution est " + solution + ")");
 
 // TODO : complétez le programme
+var proposition = "";
+var nbEssai = "";
 
-var numberOfTry = 0; //nombre d'essais
-
-while(numberOfTry <= 6){
-	numberOfTry++;
-	var yourChoise = Number(prompt("Entrez un nombre entre 1 et 100:"));
-
-	if((yourChoise > 1) || (yourChoise < 100)){
-		if(numberOfTry <= 6){
-			if(yourChoise > solution){
-				console.log(numberOfTry + ": " + yourChoise + " est trop grand");
-			}
-			else if(yourChoise < solution){
-				console.log(numberOfTry + ": " + yourChoise + " est trop petit");
-			}
-			else{
-				console.log("Bravo! La solution était " + solution +"\nVous avez trouvé la solution en " + numberOfTry + " essai(s)");
-				break;
-			}
-		}
-		else{ // Si l'utilisateur ne devine pas en 6 essais
-			console.log("Perdu... La solution était " + solution);
-		}
-	}
-	else{ //Si l'utilisateur ne rentre pas un nombre
-		if(numberOfTry > 6){
-			console.log("Perdu... La solution était " + solution);
-		}
-		else{
-			console.log(numberOfTry + ": " + "Veuillez entrer un nombre valide");
-		}
-	}
+while((proposition !== solution) && (nbEssai < 6)){
+    nbEssai++;
+    proposition = Number(prompt("Saisez votre proposition de réponse " + nbEssai + " :"));
+    if(proposition < solution){
+        console.log(proposition + " est trop petit");
+    }else if(proposition > solution){
+        console.log(proposition + " est trop grand");
+                    }
 }
+     
+                    
+if(proposition === solution){
+            console.log("Bravo !! Vous avez gagné, la solution était " + solution);
+            console.log("Vous avez trouvé en " + nbEssai + " essai(s)");
+            
+        }else{
+            console.log("Vous avez perdu... La soltuion était " +solution );
+        }
